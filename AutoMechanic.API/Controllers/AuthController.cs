1d2428e2e7371ffc6eba01d2de3026c72828590c
Victor Scott <victordscott@gmail.com>
2026-01-26 23:27:15 -0500

    progress

--- AutoMechanic.API/Controllers/AuthController.cs
+++ AutoMechanic.API/Controllers/AuthController.cs
@@ -3,19 +3,30 @@ using AutoMechanic.Auth.Models;
 using AutoMechanic.Auth.Services;
 using AutoMechanic.Auth.Services.Interfaces;
 using AutoMechanic.Common.Enums;
+using AutoMechanic.Common.Model;
 using AutoMechanic.DataAccess.DirectAccess;
 using AutoMechanic.DataAccess.EF.Models;
 using AutoMechanic.DataAccess.Models;
+using AutoMechanic.Services.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AutoMechanic.API.Controllers
 {
     [Route("[controller]/[action]")]
     [ApiController]
-    public class AuthController(IAuthService authService, IMapper mapper) : ControllerBase
+    public class AuthController(
+        IAuthService authService,
+        IUserService userService,
+        UserManager<ApplicationUser> userManager,
+        SignInManager<ApplicationUser> signInManager,
+        IPasswordHasher<ApplicationUser> passwordHasher,
+        IMapper mapper
+    ) : ControllerBase
     {
+        [AllowAnonymous]
         [HttpPost]
         public async Task<AuthResponse> Login(LoginModel loginModel)
         {
@@ -23,6 +34,7 @@ namespace AutoMechanic.API.Controllers
             return authResponse;
         }
 
+        [AllowAnonymous]
         [HttpPost]
         public async Task<AuthResponse> RefreshToken(TokenModel tokenModel)
         {
@@ -37,6 +49,7 @@ namespace AutoMechanic.API.Controllers
             return user;
         }
 
+        [AllowAnonymous]
         [HttpPost]
         public async Task<ApiResponse> GetOTPCodeForLogin(LoginModel loginModel)
         {
@@ -58,11 +71,89 @@ namespace AutoMechanic.API.Controllers
             }
         }
 
+        [AllowAnonymous]
         [HttpPost]
         public async Task<AuthResponse> LoginWithOTPCode(LoginModel loginModel)
         {
             var authResponse = await authService.LoginByOTPCode(loginModel);
             return authResponse;
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<BoolResponse> EmailAddressInUse([FromQuery] string emailAddress)
+        {
+            var user = await userManager.FindByEmailAsync(emailAddress);
+            return new BoolResponse
+            {
+                BoolValue = user is not null
+            };
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<ApiResponse> RegisterCustomer(RegistrationModel regModel)
+        {
+            var password = "Vst!1971@"; // generate a random password
+
+            var existingUser = await userManager.FindByEmailAsync(regModel.EmailAddress);
+
+            if (existingUser is not null)
+            {
+                return new ApiResponse
+                {
+                    Success = false,
+                    ErrorMessage = "User with that email address already exists. Please use a different email address",
+                    ErrorCode = (int)ApiErrorCode.RegisterEmailAddressExists
+                };
+            }
+
+            var createResult = await userManager.CreateAsync(new ApplicationUser
+            {
+                UserName = regModel.EmailAddress,
+                //NormalizedUserName = regModel.EmailAddress.ToUpper(),
+                Email = regModel.EmailAddress,
+                //NormalizedEmail = regModel.EmailAddress.ToUpper(),
+                PhoneNumber = regModel.PhoneNumber,
+                FirstName = regModel.FirstName,
+                LastName = regModel.LastName,
+                State = regModel.State,
+                Country = regModel.Country,
+                EmailConfirmed = false,
+                PhoneNumberConfirmed = false,
+                TimeZoneAbbrev = regModel.TimeZoneAbbrev,
+                DateCreated = DateTime.UtcNow
+            }, password);
+            if (createResult.Succeeded)
+            {
+                var user = await userManager.FindByNameAsync(regModel.EmailAddress);
+                var addToRoleResult = await userManager.AddToRoleAsync(user, "Customer");
+                if (addToRoleResult.Succeeded)
+                {
+                    return new ApiResponse
+                    {
+                        Success = true
+                    };
+                }
+                else
+                {
+                    return new ApiResponse
+                    {
+                        Success = false,
+                        ErrorMessage = $"Error: {addToRoleResult.Errors.First().Code} - {addToRoleResult.Errors.First().Description}",
+                        ErrorCode = (int)ApiErrorCode.SetUserRoleError
+                    };
+                }
+            }
+            else
+            {
+                return new ApiResponse
+                {
+                    Success = false,
+                    ErrorMessage = $"Error: {createResult.Errors.First().Code} - {createResult.Errors.First().Description}",
+                    ErrorCode = (int)ApiErrorCode.CreateCustomerError
+                };
+            }
         }
     }
 }
--- AutoMechanic.API/Controllers/UserController.cs
+++ AutoMechanic.API/Controllers/UserController.cs
@@ -1,7 +1,8 @@
-﻿using AutoMechanic.Auth.Helpers;
+using AutoMechanic.Auth.Helpers;
 using AutoMechanic.Auth.Models;
 using AutoMechanic.Auth.Services;
 using AutoMechanic.Common.Enums;
+using AutoMechanic.Common.Model;
 using AutoMechanic.DataAccess.DTO;
 using AutoMechanic.DataAccess.EF.Models;
 using AutoMechanic.DataAccess.Models;
@@ -16,78 +17,9 @@ namespace AutoMechanic.API.Controllers
     [Route("[controller]/[action]")]
     [ApiController]
     public class UserController(
-        IUserService userService,
-        UserManager<ApplicationUser> userManager,
-        SignInManager<ApplicationUser> signInManager,
-        IPasswordHasher<ApplicationUser> passwordHasher
+        IUserService userService
     ) : ControllerBase
     {
-        [AllowAnonymous]
-        [HttpPost]
-        public async Task<ApiResponse> RegisterCustomer(RegistrationModel regModel)
-        {
-            var password = "Vst!1971@"; // generate a random password
-
-            var existingUser = await userManager.FindByEmailAsync(regModel.EmailAddress);
-
-            if (existingUser is not null)
-            {
-                return new ApiResponse
-                {
-                    Success = false,
-                    ErrorMessage = "User with that email address already exists. Please use a different email address",
-                    ErrorCode = (int) ApiErrorCode.RegisterEmailAddressExists
-                };
-            }
-
-            var createResult = await userManager.CreateAsync(new ApplicationUser
-            {
-                UserName = regModel.EmailAddress,
-                //NormalizedUserName = regModel.EmailAddress.ToUpper(),
-                Email = regModel.EmailAddress,
-                //NormalizedEmail = regModel.EmailAddress.ToUpper(),
-                PhoneNumber = regModel.PhoneNumber,
-                FirstName = regModel.FirstName,
-                LastName = regModel.LastName,
-                State = regModel.State,
-                Country = regModel.Country,
-                EmailConfirmed = false,
-                PhoneNumberConfirmed = false,
-                TimeZoneAbbrev = regModel.TimeZoneAbbrev,
-                DateCreated = DateTime.UtcNow
-            }, password);
-            if (createResult.Succeeded)
-            {
-                var user = await userManager.FindByNameAsync(regModel.EmailAddress);
-                var addToRoleResult = await userManager.AddToRoleAsync(user, "Customer");
-                if (addToRoleResult.Succeeded)
-                {
-                    return new ApiResponse
-                    {
-                        Success = true
-                    };
-                }
-                else
-                {
-                    return new ApiResponse
-                    {
-                        Success = false,
-                        ErrorMessage = $"Error: {addToRoleResult.Errors.First().Code} - {addToRoleResult.Errors.First().Description}",
-                        ErrorCode = (int)ApiErrorCode.SetUserRoleError
-                    };
-                }
-            }
-            else
-            {
-                return new ApiResponse
-                {
-                    Success = false,
-                    ErrorMessage = $"Error: {createResult.Errors.First().Code} - {createResult.Errors.First().Description}",
-                    ErrorCode = (int)ApiErrorCode.CreateCustomerError
-                };
-            }
-        }
-
         [HttpGet]
         public async Task<UserDetail> GetProfile()
         {
