ed1f003073d7aedea0ff15bf0892908973b3ab34
Victor Scott <victordscott@gmail.com>
2026-01-29 09:57:14 -0500

    progress

--- AutoMechanic.CarAPI/Services/CarAPIService.cs
+++ AutoMechanic.CarAPI/Services/CarAPIService.cs
@@ -1,4 +1,5 @@
-﻿using AutoMechanic.CarAPI.Services.Interfaces;
+﻿using AutoMechanic.CarAPI.Models;
+using AutoMechanic.CarAPI.Services.Interfaces;
 using AutoMechanic.Configuration.Options;
 using Microsoft.Extensions.Options;
 using System;
@@ -34,6 +35,84 @@ namespace AutoMechanic.CarAPI.Services
             {
                 // TODO: logging
                 throw new Exception("CarAPI call failed");
+            }
+        }
+
+        public async Task<List<CarAPIMake>> GetMakes(string year)
+        {
+            var httpClient = httpClientFactory.CreateClient();
+            var response = await httpClient.GetAsync($"{carAPIOptions.Value.BaseUrl}api/makes/v2?year={year}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonContent = await response.Content.ReadAsStringAsync();
+                var makes = JsonSerializer.Deserialize<CarAPIMakesResponse>(jsonContent);
+                return makes.Data;
+            }
+            else
+            {
+                // TODO: logging
+                throw new Exception("CarAPI call failed");
+            }
+        }
+
+        public async Task<List<CarAPIModel>> GetModels(string year, string make)
+        {
+            var httpClient = httpClientFactory.CreateClient();
+            var response = await httpClient.GetAsync($"{carAPIOptions.Value.BaseUrl}api/models/v2?year={year}&make={make}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonContent = await response.Content.ReadAsStringAsync();
+                var models = JsonSerializer.Deserialize<CarAPIModelsResponse>(jsonContent);
+                return models.Data;
+            }
+            else
+            {
+                // TODO: logging
+                throw new Exception("CarAPI call failed");
+            }
+        }
+
+        public async Task<List<CarAPISubModel>> GetSubModels(string year, string make, string model)
+        {
+            var httpClient = httpClientFactory.CreateClient();
+            var response = await httpClient.GetAsync($"{carAPIOptions.Value.BaseUrl}api/submodels/v2?year={year}&make={make}&model={model}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonContent = await response.Content.ReadAsStringAsync();
+                var subModels = JsonSerializer.Deserialize<CarAPISubModelsResponse>(jsonContent);
+                return subModels.Data;
+            }
+            else
+            {
+                // TODO: logging
+                throw new Exception("CarAPI call failed");
+            }
+        }
+
+        public async Task<CarAPIVINResponse?> GetVIN(string vin)
+        {
+            var httpClient = httpClientFactory.CreateClient();
+            var response = await httpClient.GetAsync($"{carAPIOptions.Value.BaseUrl}api/vin/{vin}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonContent = await response.Content.ReadAsStringAsync();
+                var vinResponse = JsonSerializer.Deserialize<CarAPIVINResponse>(jsonContent);
+                return vinResponse;
+            }
+            else
+            {
+                if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    return null;
+                }
+                else
+                {
+                    throw new Exception("CarAPI call failed");
+                }
             }
         }
     }
--- AutoMechanic.CarAPI/Services/Interfaces/ICarAPIService.cs
+++ AutoMechanic.CarAPI/Services/Interfaces/ICarAPIService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutoMechanic.CarAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,9 @@ namespace AutoMechanic.CarAPI.Services.Interfaces
     public interface ICarAPIService
     {
         Task<List<int>> GetYears();
+        Task<List<CarAPIMake>> GetMakes(string year);
+        Task<List<CarAPIModel>> GetModels(string year, string make);
+        Task<List<CarAPISubModel>> GetSubModels(string year, string make, string model);
+        Task<CarAPIVINResponse?> GetVIN(string vin);
     }
 }
