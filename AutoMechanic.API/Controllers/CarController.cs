d311f8414f895387012df5a88b45cd876e1c7417
Victor Scott <victordscott@gmail.com>
2026-01-29 10:03:15 -0500

    progress

--- AutoMechanic.API/Controllers/CarController.cs
+++ AutoMechanic.API/Controllers/CarController.cs
@@ -1,7 +1,6 @@
+using AutoMechanic.CarAPI.Models;
 using AutoMechanic.CarAPI.Services.Interfaces;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System.Text.Json;
 
 namespace AutoMechanic.API.Controllers
 {
@@ -15,6 +14,31 @@ namespace AutoMechanic.API.Controllers
         public async Task<List<int>> GetYears()
         {
             return await carAPIService.GetYears();
+        }
+
+        [HttpGet]
+        public async Task<List<CarAPIMake>> GetMakes([FromQuery] string year)
+        {
+            return await carAPIService.GetMakes(year);
+        }
+
+        [HttpGet]
+        public async Task<List<CarAPIModel>> GetModels([FromQuery] string year, [FromQuery] string make)
+        {
+            return await carAPIService.GetModels(year, make);
+        }
+
+        [HttpGet]
+        public async Task<List<CarAPISubModel>> GetSubModels([FromQuery] string year, [FromQuery] string make, [FromQuery] string model)
+        {
+            return await carAPIService.GetSubModels(year, make, model);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<CarAPIVINResponse>> GetVIN([FromQuery] string vin)
+        {
+            var result = await carAPIService.GetVIN(vin);
+            return result is null ? NotFound() : result;
         }
     }
 }
